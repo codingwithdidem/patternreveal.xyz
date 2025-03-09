dee29241eed0f9b9a9f9da935af750a3b47d2e67
didemkkasslan <didem@spiky.ai>
2025-03-09 12:55:05 +0300

    Add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,30 @@
+import { FlickeringGrid } from "@/components/magicui/flickering-grid";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="relative size-[600px] w-screen h-screen overflow-hidden">
+      <FlickeringGrid
+        className="relative inset-0 z-0 [mask-image:radial-gradient(450px_circle_at_center,white,transparent)]"
+        squareSize={4}
+        gridGap={6}
+        color="#60A5FA"
+        maxOpacity={0.5}
+        flickerChance={0.1}
+        height={2800}
+        width={2800}
+      />
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="flex flex-col space-y-4">
+          <h1 className="text-6xl font-bold">404</h1>
+          <Link
+            href="/"
+            className="flex h-9 w-fit items-center justify-center rounded-md border border-black bg-black px-4 text-sm text-white hover:bg-neutral-800"
+          >
+            Go back home
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
