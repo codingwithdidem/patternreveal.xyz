38747fb299c52d2552735ac61b4f624096549e5e
didemkkasslan <didem@spiky.ai>
2025-03-09 12:31:02 +0300

    Create sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,23 @@
+import type { MetadataRoute } from "next";
+import { headers } from "next/headers";
+
+export default async function sitemap(): MetadataRoute.Sitemap {
+  const headersList = await headers();
+  const host = headersList.get("host");
+  const BASE_URL = `https://${host}`;
+
+  return [
+    {
+      url: `${BASE_URL}/`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: `${BASE_URL}/dashboard`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}
